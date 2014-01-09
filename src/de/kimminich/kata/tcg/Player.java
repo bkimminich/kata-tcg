0bf6eec8d18440c71b26592ef7b9a96ca89f810e
Björn Kimminich <bjoern.kimminich@kuehne-nagel.com>
2014-01-09 14:43:37 +0100

    added explanation how the deck array is designed

--- src/de/kimminich/kata/tcg/Player.java
+++ src/de/kimminich/kata/tcg/Player.java
@@ -5,7 +5,11 @@ public class Player {
     private int health = 30;
     private int mana = 0;
 
-    private int[] deck = new int[] {2,2,3,4,3,2,2,1,1};
+    /*
+     * The array index 0-8 represents the mana cost while the value at that
+     * index represents the number of available cards with that cost.
+     */
+    private int[] deck = new int[]{2, 2, 3, 4, 3, 2, 2, 1, 1};
 
     public int getHealth() {
         return health;
