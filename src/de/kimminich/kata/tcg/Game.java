a7af6ead8069ee7dc4bf7c8c5eaa3f11b034d8f2
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-01-09 22:42:39 +0100

    first simple test for Game class. fails from uninitialized object under test

---
+++ src/de/kimminich/kata/tcg/Game.java
@@ -0,0 +1,16 @@
+package de.kimminich.kata.tcg;
+
+public class Game {
+
+    private Player player1;
+    private Player player2;
+
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+    }
+
+}
---
+++ test/de/kimminich/kata/tcg/GameTest.java
@@ -0,0 +1,21 @@
+package de.kimminich.kata.tcg;
+
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+public class GameTest {
+
+    private Game game;
+
+    @Test
+    public void gameShouldHaveTwoPlayers() {
+        assertThat(game.getPlayer1(), is(notNullValue()));
+        assertThat(game.getPlayer2(), is(notNullValue()));
+    }
+
+}
