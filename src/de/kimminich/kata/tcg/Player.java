0e7990f512fda02e62a30ca6025855a87906c04d
Björn Kimminich <bjoern.kimminich@kuehne-nagel.com>
2014-01-09 14:00:31 +0100

    added player class and test class with setup and noop test method

---
+++ src/de/kimminich/kata/tcg/Player.java
@@ -0,0 +1,4 @@
+package de.kimminich.kata.tcg;
+
+public class Player {
+}
---
+++ test/de/kimminich/kata/tcg/PlayerTest.java
@@ -0,0 +1,21 @@
+package de.kimminich.kata.tcg;
+
+import de.kimminich.kata.tcg.Player;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlayerTest {
+
+    Player player;
+
+    @Before
+    public void setUp() {
+        player = new Player();
+    }
+
+    @Test
+    public void playerShouldHave30InitialHealth() {
+
+    }
+
+}
