3770115a124c5b3f5f4070e4047c65213cd506ee
Björn Kimminich <bjoern.kimminich@kuehne-nagel.com>
2014-01-10 15:18:21 +0100

    fixed "given" part of tests, eliminated useless variables, added syntactic sugar

--- test/de/kimminich/kata/tcg/GameTest.java
+++ test/de/kimminich/kata/tcg/GameTest.java
@@ -1,9 +1,7 @@
 package de.kimminich.kata.tcg;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -15,85 +13,84 @@ public class GameTest {
 
     private Game game;
 
-    private Player player1;
-
-    private Player player2;
-
-    @Mock
-    private CardPicker player1CardPicker;
-
-    @Mock
-    private CardPicker player2CardPicker;
-
-    @Before
-    public void setUp() {
-        player1 = new Player(player1CardPicker);
-        player2 = new Player(player2CardPicker);
-        game = new Game(player1, player2);
-    }
-
     @Test
     public void gameShouldHaveTwoPlayers() {
+        game = new Game(aPlayer(), aPlayer());
+
         assertThat(game.getPlayer1(), is(notNullValue()));
         assertThat(game.getPlayer2(), is(notNullValue()));
     }
 
     @Test
     public void eachPlayerShouldHaveStartingHandOfThreeCardsFromHisDeck() {
-        assertThat(game.getPlayer1().getNumberOfHandCards(), is(equalTo(3)));
-        assertThat(game.getPlayer1().getNumberOfDeckCards(), is(equalTo(17)));
-        assertThat(game.getPlayer2().getNumberOfHandCards(), is(equalTo(3)));
-        assertThat(game.getPlayer2().getNumberOfDeckCards(), is(equalTo(17)));
+        game = new Game(aPlayer(), aPlayer());
+
+        assertThat(player1().getNumberOfHandCards(), is(equalTo(3)));
+        assertThat(player1().getNumberOfDeckCards(), is(equalTo(17)));
+        assertThat(player2().getNumberOfHandCards(), is(equalTo(3)));
+        assertThat(player2().getNumberOfDeckCards(), is(equalTo(17)));
     }
 
     @Test
     public void activePlayerShouldSwitchOnEndOfTurn() {
-        game.setActivePlayer(player1);
+        game = new Game(aPlayer(), aPlayer());
+        game.setActivePlayer(player1());
+
         game.endTurn();
-        assertThat(game.getActivePlayer(), is(player2));
+        assertThat(game.getActivePlayer(), is(player2()));
         game.endTurn();
-        assertThat(game.getActivePlayer(), is(player1));
+        assertThat(game.getActivePlayer(), is(player1()));
     }
 
     @Test
     public void activePlayerShouldReceiveOneManaSlotOnBeginningOfTurn() {
-        player1 = aPlayer().withManaSlots(0);
-        game = new Game(player1, aPlayer());
-        game.setActivePlayer(player1);
+        game = new Game(aPlayer().withManaSlots(0), aPlayer());
+        game.setActivePlayer(player1());
+
         game.beginTurn();
-        assertThat(player1.getManaSlots(), is(equalTo(1)));
+
+        assertThat(player1().getManaSlots(), is(equalTo(1)));
     }
 
     @Test
     public void activePlayerShouldRefillManaOnBeginningOfTurn() {
-        player1 = aPlayer().withManaSlots(3).withMana(0);
-        game = new Game(player1, aPlayer());
-        game.setActivePlayer(player1);
+        game = new Game(aPlayer().withManaSlots(3).withMana(0), aPlayer());
+        game.setActivePlayer(player1());
+
         game.beginTurn();
-        assertThat(player1.getMana(), is(equalTo(player1.getManaSlots())));
+
+        assertThat(player1().getMana(), is(equalTo(player1().getManaSlots())));
     }
 
     @Test
     public void activePlayerShouldDrawCardOnBeginningOfTurn() {
-        player1 = spy(player1);
-        game = new Game(player1, aPlayer());
-        game.setActivePlayer(player1);
+        game = new Game(spy(aPlayer()), aPlayer());
+        game.setActivePlayer(player1());
+
         game.beginTurn();
-        verify(player1, times(3+1)).drawCard();
+
+        verify(player1(), times(3 + 1)).drawCard();
     }
 
     @Test
     public void playerWithOneHealthAndEmptyDeckShouldDieFromBleedingOutOnBeginningOfTurn() {
-        player1 = aPlayer().withHealth(1).withNoCardsInDeck();
-        player2 = aPlayer();
-        game = new Game(player1, player2);
-        game.setActivePlayer(player1);
+        game = new Game(aPlayer().withHealth(1).withNoCardsInDeck(), aPlayer());
+        game.setActivePlayer(player1());
+
         game.beginTurn();
-        assertThat(game.getWinner(), is(player2));
+
+        assertThat(game.getWinner(), is(player2()));
     }
 
     private FakePlayer aPlayer() {
         return new FakePlayer(mock(CardPicker.class));
     }
 
+    private Player player1() {
+        return game.getPlayer1();
+    }
+
+    private Player player2() {
+        return game.getPlayer2();
+    }
 }
