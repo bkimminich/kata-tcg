d565c8c83db77f04c96cb02104ae34cdbee67fc2
Björn Kimminich <bjoern.kimminich@kuehne-nagel.com>
2014-01-10 14:45:11 +0100

    removed unused import

--- test/de/kimminich/kata/tcg/GameTest.java
+++ test/de/kimminich/kata/tcg/GameTest.java
@@ -4,7 +4,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
