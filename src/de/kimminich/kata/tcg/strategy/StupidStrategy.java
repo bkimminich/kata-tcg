66449dec6c375f28c2b77247029b9f93628fcf13
Björn Kimminich <bjoern.kimminich@kuehne-nagel.com>
2014-01-14 15:11:13 +0100

    implemented stupid strategy

--- src/de/kimminich/kata/tcg/strategy/StupidStrategy.java
+++ src/de/kimminich/kata/tcg/strategy/StupidStrategy.java
@@ -1,8 +1,10 @@
 package de.kimminich.kata.tcg.strategy;
 
+import static java.util.Arrays.*;
+
 public class StupidStrategy implements Strategy {
 
     public int nextCard(int... availableCards) {
-        return -1;
+        return stream(availableCards).min().getAsInt();
     }
 }
