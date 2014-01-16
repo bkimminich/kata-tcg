316409982ec43112d432daad96dfe4d38af35bbb
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-01-16 20:06:22 +0100

    sweetened console input code

--- test/de/kimminich/kata/tcg/strategy/ConsoleInputStrategyTest.java
+++ test/de/kimminich/kata/tcg/strategy/ConsoleInputStrategyTest.java
@@ -21,7 +21,7 @@ public class ConsoleInputStrategyTest {
     @Test
     public void manualInputStrategyShouldPlayCardsSelectedOnSystemConsole() {
         strategy = new ConsoleInputStrategy();
-        consoleInput.provideText("2\n");
+        consoleInput.provideText(player().enter("2").sequentially());
 
         assertThat(strategy.nextCard(10, Card.list(0, 2, 3)), is(card(2)));
     }
@@ -29,7 +29,7 @@ public class ConsoleInputStrategyTest {
     @Test
     public void willRejectTooExpensiveCardsUntilAffordableCardIsChosen() {
         strategy = new ConsoleInputStrategy();
-        consoleInput.provideText("8\n7\n6\n");
+        consoleInput.provideText(player().enter("8").enter("7").enter("6").sequentially());
 
         assertThat(strategy.nextCard(6, Card.list(6, 7, 8)), is(card(6)));
     }
@@ -37,7 +37,7 @@ public class ConsoleInputStrategyTest {
     @Test
     public void willRejectCardsNotPresentInHandUntilHandCardIsChosen() {
         strategy = new ConsoleInputStrategy();
-        consoleInput.provideText("1\n2\n3\n");
+        consoleInput.provideText(player().enter("1").enter("2").enter("3").sequentially());
 
         assertThat(strategy.nextCard(5, Card.list(3, 4, 5)), is(card(3)));
     }
@@ -45,9 +45,26 @@ public class ConsoleInputStrategyTest {
     @Test
     public void willRejectInvalidInputUntilValidCardIsChosen() {
         strategy = new ConsoleInputStrategy();
-        consoleInput.provideText("-1\n9\n666\nabc\n5\n");
+        consoleInput.provideText(player().enter("-1").enter("9").enter("666").enter("abc").enter("5").sequentially());
 
         assertThat(strategy.nextCard(10, Card.list(5)), is(card(5)));
+    }
+
+    private ConsoleInputBuilder player() {
+        return new ConsoleInputBuilder();
+    }
+
+    private final class ConsoleInputBuilder {
+        StringBuilder buf = new StringBuilder();
+
+        public ConsoleInputBuilder enter(String in) {
+            buf.append(in).append("\n");
+            return this;
+        }
+
+        public String sequentially() {
+            return buf.toString();
+        }
     }
 
     private Optional<Card> card(int card) {
