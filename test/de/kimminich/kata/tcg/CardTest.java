42c6d2f2d2718a0e40fab5642bb1164a37cabea9
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-01-25 22:59:45 +0100

    added readability sugar added test case for 0 cost card

--- test/de/kimminich/kata/tcg/CardTest.java
+++ test/de/kimminich/kata/tcg/CardTest.java
@@ -9,15 +9,20 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class CardTest {
 
     @Test
-    public void cardShouldCauseDamageEqualToManaCost() {
-        assertThat(new Card(1).getDamage(), is(equalTo(1)));
-        assertThat(new Card(2).getDamage(), is(equalTo(2)));
-        assertThat(new Card(3).getDamage(), is(equalTo(3)));
-        assertThat(new Card(4).getDamage(), is(equalTo(4)));
-        assertThat(new Card(5).getDamage(), is(equalTo(5)));
-        assertThat(new Card(6).getDamage(), is(equalTo(6)));
-        assertThat(new Card(7).getDamage(), is(equalTo(7)));
-        assertThat(new Card(8).getDamage(), is(equalTo(8)));
+    public void cardsShouldCauseDamageEqualToManaCost() {
+        assertThat(aCardWithManaCost(0).getDamage(), is(equalTo(0)));
+        assertThat(aCardWithManaCost(1).getDamage(), is(equalTo(1)));
+        assertThat(aCardWithManaCost(2).getDamage(), is(equalTo(2)));
+        assertThat(aCardWithManaCost(3).getDamage(), is(equalTo(3)));
+        assertThat(aCardWithManaCost(4).getDamage(), is(equalTo(4)));
+        assertThat(aCardWithManaCost(5).getDamage(), is(equalTo(5)));
+        assertThat(aCardWithManaCost(6).getDamage(), is(equalTo(6)));
+        assertThat(aCardWithManaCost(7).getDamage(), is(equalTo(7)));
+        assertThat(aCardWithManaCost(8).getDamage(), is(equalTo(8)));
+    }
+
+    private Card aCardWithManaCost(int manaCost) {
+        return new Card(manaCost);
     }
 
 }
