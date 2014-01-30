1d9987e6646c208c8a60ddb19c329f2c3fb13eb0
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-01-30 22:28:25 +0100

    removed redundant object creation

--- src/de/kimminich/kata/tcg/strategy/AiStrategy.java
+++ src/de/kimminich/kata/tcg/strategy/AiStrategy.java
@@ -8,27 +8,30 @@ import java.util.Optional;
 
 public class AiStrategy implements Strategy {
 
+    Strategy lowestCardStrategy = new LowestCardFirstStrategy();
+    Strategy highestCardStrategy = new HighestCardFirstStrategy();
+
     @Override
     public Optional<Card> nextCard(int mana, List<Card> availableCards) {
         if (availableCards.size() > 2) {
             if (canAffordMoreThanOneCard(mana, availableCards)) {
-                return new LowestCardFirstStrategy().nextCard(mana, availableCards);
+                return lowestCardStrategy.nextCard(mana, availableCards);
             } else {
-                return new HighestCardFirstStrategy().nextCard(mana, availableCards);
+                return highestCardStrategy.nextCard(mana, availableCards);
             }
         }
         if (availableCards.size() == 2) {
-            return new HighestCardFirstStrategy().nextCard(mana, availableCards);
+            return highestCardStrategy.nextCard(mana, availableCards);
         }
         return Optional.empty();
     }
 
     private boolean canAffordMoreThanOneCard(int mana, List<Card> availableCards) {
-        Optional<Card> card = new LowestCardFirstStrategy().nextCard(mana, availableCards);
+        Optional<Card> card = lowestCardStrategy.nextCard(mana, availableCards);
         if (card.isPresent()) {
             List<Card> remainingCards = new ArrayList<>(availableCards);
             remainingCards.remove(card.get());
-            return new LowestCardFirstStrategy().nextCard(mana - card.get().getManaCost(), remainingCards).isPresent();
+            return lowestCardStrategy.nextCard(mana - card.get().getManaCost(), remainingCards).isPresent();
         }
         return false;
     }
