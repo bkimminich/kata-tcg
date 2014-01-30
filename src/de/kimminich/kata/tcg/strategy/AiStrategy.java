c2edc32669142e8b6f46edb24ca621af44db6d0d
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-01-30 12:39:04 +0100

    implemented maximized damage

--- src/de/kimminich/kata/tcg/strategy/AiStrategy.java
+++ src/de/kimminich/kata/tcg/strategy/AiStrategy.java
@@ -2,6 +2,8 @@ package de.kimminich.kata.tcg.strategy;
 
 import de.kimminich.kata.tcg.Card;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,11 +12,27 @@ public class AiStrategy implements Strategy {
     @Override
     public Optional<Card> nextCard(int mana, List<Card> availableCards) {
         if (availableCards.size() > 2) {
-            return new LowestCardFirstStrategy().nextCard(mana, availableCards);
+            if (canAffordMoreThanOneCard(mana, availableCards)) {
+                return new LowestCardFirstStrategy().nextCard(mana, availableCards);
+            }
+            else {
+                return new HighestCardFirstStrategy().nextCard(mana, availableCards);
+            }
         }
         if (availableCards.size() == 2) {
             return new HighestCardFirstStrategy().nextCard(mana, availableCards);
         }
         return Optional.empty();
     }
+
+    private boolean canAffordMoreThanOneCard(int mana, List<Card> availableCards) {
+        Optional<Card> card = new LowestCardFirstStrategy().nextCard(mana, availableCards);
+        if (card.isPresent()) {
+            List<Card> remainingCards = new ArrayList<>(availableCards);
+            remainingCards.remove(card.get());
+            return new LowestCardFirstStrategy().nextCard(mana-card.get().getManaCost(), remainingCards).isPresent();
+        }
+        return false;
+    }
+
 }
