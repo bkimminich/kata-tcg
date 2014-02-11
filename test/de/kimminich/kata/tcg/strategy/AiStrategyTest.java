ea56c5264cb51c6625677b60e2880692252d5f15
Björn Kimminich <bjoern.kimminich@kuehne-nagel.com>
2014-02-11 16:21:24 +0100

    simplified test cases

--- test/de/kimminich/kata/tcg/strategy/AiStrategyTest.java
+++ test/de/kimminich/kata/tcg/strategy/AiStrategyTest.java
@@ -21,26 +21,18 @@ public class AiStrategyTest {
     }
 
     @Test
-    public void shouldTryToMaximizeDamageOutputInCurrentTurn() {
+    public void shouldMaximizeDamageOutputInCurrentTurn() {
         assertThat(strategy.nextCard(withMana(8), fromCards(7, 6, 4, 3, 2)), either(is(card(2))).or(is(card(6))));
     }
 
     @Test
-    public void shouldTryToReduceHandSizeToTwo() {
-        assertThat(strategy.nextCard(withMana(4), fromCards(1, 1, 1, 1, 3)), is(card(1)));
-        assertThat(strategy.nextCard(withMana(3), fromCards(1, 1, 1, 3)), is(card(1)));
-        assertThat(strategy.nextCard(withMana(2), fromCards(1, 1, 3)), is(card(1)));
+    public void shouldPlayAsManyCardsAsPossibleForMaximumDamage() {
+        assertThat(strategy.nextCard(withMana(3), fromCards(1, 2, 3)), either(is(card(1))).or(is(card(2))));
     }
 
     @Test
-    public void shouldPickHighestAffordableCardFromHandOfSizeTwo() {
-        assertThat(strategy.nextCard(withMana(6), fromCards(4, 5)), is(card(5)));
-        assertThat(strategy.nextCard(withMana(1), fromCards(1, 2)), is(card(1)));
-    }
-
-    @Test
-    public void shouldPlayLastCardIfAffordable() {
-        assertThat(strategy.nextCard(withMana(6), fromCards(5)), is(card(5)));
+    public void shouldPickHighestAffordableCardWhenNoComboIsPossible() {
+        assertThat(strategy.nextCard(withMana(2), fromCards(1, 2, 3)), is(card(2)));
     }
 
     @Test
