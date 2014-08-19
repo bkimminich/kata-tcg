1852e671b7576fa1bc363231b1960b015ae5290f
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-08-19 21:33:13 +0200

    added more player properties

--- tcg-js/js/player.js
+++ tcg-js/js/player.js
@@ -1,5 +1,8 @@
 if (typeof NS == 'undefined') { NS = {}; }
 
 NS.player = {
-    health: 30
+    health: 30,
+    mana: 0,
+    maxMana: 0,
+    hand: []
 }
--- tcg-js/js/tests/player.test.js
+++ tcg-js/js/tests/player.test.js
@@ -4,4 +4,16 @@ describe("A Player", function() {
     it("should have an initial health of 30", function() {
         expect(player.health).toBe(30);
     });
+
+    it("should have an initial mana of zero", function() {
+        expect(player.mana).toBe(0);
+    });
+
+    it("should have an initial maximum mana of zero", function() {
+        expect(player.maxMana).toBe(0);
+    });
+
+    it("should have no cards in hand", function() {
+        expect(player.hand.length).toBe(0);
+    });
 });
