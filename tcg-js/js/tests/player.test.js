1d8990ae901bbb4122a900dfdce53c4bdfe78a0d
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-08-19 21:41:30 +0200

    added deck property

--- tcg-js/js/tests/player.test.js
+++ tcg-js/js/tests/player.test.js
@@ -13,7 +13,12 @@ describe("A Player", function() {
         expect(player.maxMana).toBe(0);
     });
 
+    it("should have the 20 default cards in deck", function() {
+        expect(player.deck).toEqual([0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8]);
+    });
+
     it("should have no cards in hand", function() {
         expect(player.hand.length).toBe(0);
     });
+
 });
