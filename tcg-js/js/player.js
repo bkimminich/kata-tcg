a0c0c4a19ebe90de073fc99bc229a2d71cd13862
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-08-20 10:10:38 +0200

    using function expression to create players

--- tcg-js/js/player.js
+++ tcg-js/js/player.js
@@ -1,9 +1,11 @@
 if (typeof NS == 'undefined') { NS = {}; }
 
-NS.player = {
-    health: 30,
-    mana: 0,
-    maxMana: 0,
-    deck: [0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8],
-    hand: []
+NS.player = function() {
+    return {
+        health: 30,
+            mana: 0,
+        maxMana: 0,
+        deck: [0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8],
+        hand: []
+    }
 }
--- tcg-js/js/tests/player.test.js
+++ tcg-js/js/tests/player.test.js
@@ -1,5 +1,5 @@
 describe("A Player", function() {
-    var player = NS.player;
+    var player = NS.player();
 
     it("should have an initial health of 30", function() {
         expect(player.health).toBe(30);
