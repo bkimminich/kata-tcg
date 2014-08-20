53d232b43f6c26fdae18f7b7f22f638b5e4f8806
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-08-20 23:57:51 +0200

    moved drawCard() into player object

--- tcg-js/js/player.js
+++ tcg-js/js/player.js
@@ -8,18 +8,15 @@ NS.player = function () {
         mana: 0,
         maxMana: 0,
         deck: [0, 0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 6, 6, 7, 8],
-        hand: []
-    }
-
-    instance.drawCard = function() {
-        if (this.deck.length === 0) {
-            this.health--;
-        } else {
-            var cardIndex = Math.floor(Math.random( ) * this.deck.length );
-            this.hand.push(this.deck.splice(cardIndex, 1));
+        hand: [],
+        drawCard : function() {
+            if (this.deck.length === 0) {
+                this.health--;
+            } else {
+                var cardIndex = Math.floor(Math.random( ) * this.deck.length );
+                this.hand.push(this.deck.splice(cardIndex, 1));
+            }
         }
-    };
-
-    return instance;
-
+    }
+   return instance;
 }
