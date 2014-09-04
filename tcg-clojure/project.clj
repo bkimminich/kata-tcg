6fbf5f7f1425359c03c1a3ea15103960386b6b77
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-09-04 23:41:10 +0200

    set version 1.3.1 for tagging

--- tcg-clojure/project.clj
+++ tcg-clojure/project.clj
@@ -1,4 +1,4 @@
-(defproject tcg-clojure "1.4"
+(defproject tcg-clojure "1.3.1"
   :description "Clojure implementation of the Trading Card Game Kata"
   :url "https://github.com/bkimminich/kata-tcg"
   :main tcg-clojure.core
