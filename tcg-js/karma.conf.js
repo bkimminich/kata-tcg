f33680ec2c001b120270f4872a64b94b2a8a6c11
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-09-14 00:23:27 +0200

    added code coverage reporting

--- tcg-js/karma.conf.js
+++ tcg-js/karma.conf.js
@@ -28,13 +28,19 @@ module.exports = function (config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
+            '**/js/*.js': 'coverage'
         },
 
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['progress', 'coverage'],
+
+        coverageReporter: {
+            type: 'lcov',
+            dir: 'coverage/'
+        },
 
 
         // web server port
