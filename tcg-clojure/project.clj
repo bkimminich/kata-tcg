5e3153535d39a5409c7c692730a5b84137e91870
Björn Kimminich <bjoern.kimminich@gmx.de>
2015-08-18 13:44:36 +0200

    dependencies updated

--- tcg-clojure/project.clj
+++ tcg-clojure/project.clj
@@ -1,4 +1,4 @@
 (defproject tcg-clojure "1.4.0"
   :description "Clojure implementation of the Trading Card Game Kata"
   :url "https://github.com/bkimminich/kata-tcg"
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]])
