83dc1f0bf2c55e902431e4be75d540a1ed56fe4e
Björn Kimminich <bjoern.kimminich@gmx.de>
2015-08-19 11:40:52 +0200

    refined invalid input test

--- tcg-groovy/src/test/groovy/de/kimminich/groovy/tcg/PlayerSpec.groovy
+++ tcg-groovy/src/test/groovy/de/kimminich/groovy/tcg/PlayerSpec.groovy
@@ -107,7 +107,7 @@ class PlayerSpec extends Specification {
     def "invalid choices for card to play should not be accepted"() {
         given:
         OptionPane optionPane = Mock(OptionPane)
-        optionPane.showInputDialog(_ as String) >>> ["a", "4", "3", null]
+        optionPane.showInputDialog(_ as String) >>> ["aa", "abc", "a4", "a3", "x3", null]
         and:
         player = new Player(hand: [1, 2, 3], mana: 2, optionPane: optionPane)
 
@@ -121,7 +121,7 @@ class PlayerSpec extends Specification {
     def "player can play multiple cards as long as he has sufficient mana"() {
         given:
         OptionPane optionPane = Mock(OptionPane)
-        optionPane.showInputDialog(_ as String) >>> ["1", "2", null]
+        optionPane.showInputDialog(_ as String) >>> ["a1", "a2", null]
         and:
         player = new Player(hand: [1, 2, 5], mana: 3, optionPane: optionPane)
 
